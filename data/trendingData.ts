6c0f0bf3a11f3b99d36dd6bef18eb04ec6ac7532
Kumar Ganesh Magam <76594791+kumarganeshmagam@users.noreply.github.com>
2025-10-16 17:50:33 +0530

    Update trending items to latest models

--- data/trendingData.ts
+++ data/trendingData.ts
@@ -1,10 +1,10 @@
 export const trendingItems = [
-  { title: 'iPhone 15 Pro Max', badge: 'FlagShip' },
-  { title: 'OnePlus 12', badge: 'Flagship Killer' },
-  { title: 'Google Pixel 8a', badge: 'All Rounder' },
-  { title: 'Samsung Galaxy A55', badge: 'Most Voted' },
-  { title: 'Nothing Phone (2a)', badge: 'Popular' },
-  { title: 'Asus ROG Phone 8', badge: 'Best Gaming' },
-  { title: 'Moto G Power (2024)', badge: 'Budget' },
-  { title: 'Xiaomi 14', badge: 'New Release' },
+  { title: 'iPhone 17 Pro Max', badge: 'FlagShip' },
+  { title: 'OnePlus 13', badge: 'Flagship Killer' },
+  { title: 'Samsung S25 Ultra', badge: 'All Rounder' },
+  { title: 'Samsung S24 Ultra', badge: 'Most Voted' },
+  { title: 'Google Pixcel 10 Pro Max', badge: 'Popular' },
+  { title: 'Asus ROG Phone 9 Pro', badge: 'Best Gaming' },
+  { title: 'Moto Edge 60 Fusion', badge: 'Budget' },
+  { title: 'Vivo V60 E', badge: 'New Release' },
 ];
